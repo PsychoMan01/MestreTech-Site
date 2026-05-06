4529cd881c5c835563640f33ac5bc3a574888178
danielbritocardoso <danielbritocardoso850@gmail.com>
2026-05-06 18:22:14 -0100

    correcao ne php

--- config/enviar_mensagem.php
+++ config/enviar_mensagem.php
@@ -5,15 +5,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //emos e quel real_escape_string te protege contra SQL injection
     $nome = $conn->real_escape_string($_POST['nome']);
     $email = $conn->real_escape_string($_POST['email']);
-    $assunto = $assunto->real_escape_string($_POST['assunto'] ?? ''); // li pega assunto sel ca existi ele te usa string vazia
-     $mensagem = $mensagem->real_escape_string($_POST['mensagem']);
+    $assunto = $conn->real_escape_string($_POST['assunto'] ?? ''); // Se assunto não existir, usa string vazia
+    $mensagem = $conn->real_escape_string($_POST['mensagem']);
 
     $sql = "INSERT INTO mensagens (nome, email, assunto, mensagem) VALUES ('$nome', '$email', '$assunto', '$mensagem')";
 
     if ($conn->query($sql) === TRUE){   //li te executa querry e verifica sel funciona com sucesso
         echo "Sucedidd";
     } else {
-        echo "error: "; 
+        echo "error: " . $conn->error;
     }
 }
 $conn->close();
